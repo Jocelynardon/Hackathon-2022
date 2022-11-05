4ece9ffd2d5a7890f0d2c45f0e8fd3e1cbdf0220
Jocelynardon <ozelynardon@gmail.com>
2022-11-05 15:09:37 -0600

    1er. Problema
    Resolución del primer problema

--- Hackathon2022/Program.cs
+++ Hackathon2022/Program.cs
@@ -7,6 +7,53 @@ namespace Hackathon2022
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese la primera cadena: ");
+            string cadena1 = Console.ReadLine().ToLower();
+            Console.WriteLine("Ingrese la segunda cadena: ");
+            string cadena2 = Console.ReadLine().ToLower();
+            if (cadena1.Length == cadena2.Length)
+            {
+                bool desordenado= cadenaDesordenada(cadena1, cadena2);
+                if (desordenado)
+                {
+                    Console.WriteLine("La primera cadena es una cadena " +
+                        "desordenada de la segunda");
+                }
+                else
+                {
+                    Console.WriteLine("La primera cadena NO es una cadena " +
+                        "desordenada de la segunda");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Las cadenas no tienen la misma longitud");
+            }
         }
+
+        public static bool cadenaDesordenada(string cadena1, string cadena2)
+        {
+            bool encontrado = false;
+            string temp = "";
+            for (int i = 0; i < cadena1.Length; i++)
+            {
+                encontrado = false;
+                for (int j = 0; j < cadena2.Length; j++)
+                {
+                    if (cadena1[i] == cadena2[j])
+                    {
+                        temp = cadena2.Remove(j,1);
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (!encontrado)
+                {
+                    return false;
+                }
+                cadena2 = temp;
+            }
+            return true;
+        }  
     }
 }
